5a58e75c6156afbd70d7395a3babc8969722b213
Kerim <kerimomerbegovic@hotmail.com>
2022-05-22 20:14:32 +0200

    Created operate function

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ const add = (a,b) => { return a+b; }
 const subtract = (a,b) => { return a-b;}
 const multiply = (a,b) => { return a*b; }
 const divide = (a,b) => { return a == 0 || b == 0 ? 'Error' : a/b } 
+
+let operate = (operation,a,b) => {
+    switch (operation){
+        case 'adding':
+           return add(a,b);
+        case 'subtracting':
+           return subtract(a,b);
+        case 'dividing':
+           return divide(a,b);
+        case 'multiplying':
+          return multiply(a,b);
+    }
+}
