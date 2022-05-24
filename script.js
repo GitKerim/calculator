106b21d6c60e69f00e54fdbcd6dd8092f627a5f1
Kerim <kerimomerbegovic@hotmail.com>
2022-05-24 19:43:51 +0200

    Added if statement in function which adds numbers to display

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 let numbers = document.querySelectorAll('[type="number"]');
-let number1 
+let number1 = 0
 let operant
 let number2
 let display = document.getElementById('maindisplay');
 
 numbers.forEach(number => number.addEventListener("click", (e) =>{
-number1 = e.target.innerText; 
-display.innerText = number1;
-}));
+number1.toString()[0] == 0 ? number1 = e.target.innerText : number1 = number1.toString() + e.target.innerText;
+
+display.innerText = number1
+}
+));
 const add = (a,b) => { return a+b; } 
 const subtract = (a,b) => { return a-b;}
 const multiply = (a,b) => { return a*b; }
