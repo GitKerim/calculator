b20d3db3f2d2a308476fa986a5ef7e7d6bd26405
Kerim <kerimomerbegovic@hotmail.com>
2022-05-24 17:34:03 +0200

    Added function that writes value from clicked number to display

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+let numbers = document.querySelectorAll('[type="number"]');
+let number1 
+let operant
+let number2
+let display = document.getElementById('maindisplay');
+
+numbers.forEach(number => number.addEventListener("click", (e) =>{
+number1 = e.target.innerText; 
+display.innerText = number1;
+}));
 const add = (a,b) => { return a+b; } 
 const subtract = (a,b) => { return a-b;}
 const multiply = (a,b) => { return a*b; }
