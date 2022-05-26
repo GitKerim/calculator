b1d7b48ed3ab9b0339d5b12d5ccbb497a94312f5
Kerim <kerimomerbegovic@hotmail.com>
2022-05-26 13:43:07 +0200

    Added funcionality to equals button and fixed backspace button funcionality

--- script.js
+++ script.js
@@ -5,13 +5,22 @@ let clear = document.getElementById('clear');
 let signChange = document.getElementById('signchange');
 let backSpace = document.getElementById('backspace');
 let dot = document.getElementById('dot');
+let equals = document.getElementById('equals')
+
 let displayText = 0;
 let number1;
 let number2 = null;
 let sign = null;
-let result;
 let numLength;
 let textLength;
+
+equals.addEventListener('click', () => {
+   operate(sign,number1,number2)
+   number2 = null
+   sign = null;
+   displayText = number1;
+   display.innerText = displayText;
+})
 
 operants.forEach(operant => operant.addEventListener('click', (e) =>{
    if(sign == null){
@@ -25,10 +34,9 @@ operants.forEach(operant => operant.addEventListener('click', (e) =>{
       display.innerText = displayText;
    }else{
       operate(sign,number1,number2)
-      number1 = result;
       number2 = null
       sign = e.target.innerText;
-      displayText = result + sign;
+      displayText = number1 + sign;
       display.innerText = displayText;
    }
 
@@ -46,13 +54,14 @@ if(displayText.toString()[0] == 0 && displayText.toString()[1] != '.'){
    display.innerText = displayText
 }else{
    displayText = displayText + e.target.innerText;
+   number1 = displayText
    display.innerText = displayText;
 }
 }));
 
 clear.addEventListener("click",  () => {
    displayText = 0;
-   number2 = 0;
+   number2 = null;
    display.innerText = displayText;
    sign = null
    dot.disabled = false;
@@ -74,12 +83,32 @@ dot.addEventListener('click', () => {
 }); 
 
 backSpace.addEventListener('click', () =>{
-   if (displayText === "" || displayText == 0) {
-   displayText = 0
-   display.innerText = displayText;
-   }else{
-   displayText = displayText.slice(0, -1); 
-   display.innerText = displayText;
+   if (displayText.length == 1 || displayText == 0) {
+      displayText = 0
+      number1 = 0
+      display.innerText = displayText;
+   }else if(number2 != null && number2.length != 1){
+      numLength = number1.toString().length;
+      textLength = displayText.toString().length;
+      displayText = displayText.toString().slice(0, -1);
+      number2 = displayText.toString().substring(numLength + 1,textLength);
+      display.innerText = displayText;
+   }else if(sign != null && displayText.slice(0, -1) == '+'||'-'||'*'||'/'){
+      sign = null;
+      displayText = displayText.toString().slice(0, -1);
+      number1 = displayText;
+      display.innerText = displayText;
+      number2 = null
+   }else if(number2 != null){
+      numLength = number1.toString().length;
+      textLength = displayText.toString().length;
+      displayText = displayText.toString().slice(0, -1);
+      number2 = displayText.toString().substring(numLength + 1,textLength);
+      display.innerText = displayText;
+   }else if(number1 == displayText && sign == null){
+      displayText = displayText.toString().slice(0, -1);
+      number1 = displayText;
+      display.innerText = displayText;
    }
 });
 
@@ -91,16 +120,16 @@ const divide = (a,b) => { return a == 0 || b == 0 ? 'Error' : a/b }
 let operate = (operation,a,b) => {
     switch (operation){
         case '+':
-           result = add(parseInt(a),parseInt(b));
-           return result
+          number1 = add(parseInt(a),parseInt(b));
+           return number1
         case '-':
-         result = subtract(a,b);
-           return result
+       number1 = subtract(a,b);
+           return number1
         case '/':
-         result = divide(a,b);
-           return result
+       number1 = divide(a,b);
+           return number1
         case '*':
-         result = multiply(a,b);
-          return result
+       number1 = multiply(a,b);
+          return number1
     }
 }
